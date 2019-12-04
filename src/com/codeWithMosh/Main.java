bb7b685ef9e92d9fff840a135e859c5024c9a14c
vishnu <vvichugit@gmail.com>
2019-12-03 18:34:07 -0600

    initial commit

---
+++ src/com/codeWithMosh/Main.java
@@ -0,0 +1,15 @@
+package com.codeWithMosh;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Mp3 or Music List:
+        //A user can add a song to the playList:
+        //can shuffle between songs.
+        //add/delete a song from playList.
+
+
+
+    }
+}
---
+++ src/com/codeWithMosh/Song.java
@@ -0,0 +1,13 @@
+package com.codeWithMosh;
+
+public class Song {
+
+    private String name;
+    private double duration;
+
+    public Song(String name, double duration){
+        this.name = name;
+        this.duration = duration;
+    }
+
+}
